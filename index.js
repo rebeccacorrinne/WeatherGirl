03c452feca425ca1a5520192c248cc903aee41fa
rebeccacorrinne <rebeccacorrinne@gmail.com>
2026-04-29 19:15:23 -0400

    fixed main icon and adjust padding and spacing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
+let iconElement = document.querySelector(".icon");
 
     let temperature = response.data.temperature.current;
     let city = response.data.city;
@@ -16,7 +16,8 @@ timeElement.innerHTML = formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
-temperatureElement.innerHTML = Math.round(temperature);  
+temperatureElement.innerHTML = Math.round(temperature);
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;  
 
 getForecast(response.data.city);
 }
@@ -48,6 +49,13 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "5aebaf322a84ft22eb0f046oe39fc614";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -63,7 +71,7 @@ response.data.daily.forEach(function (day, index) {
 forecastHtml = 
 forecastHtml + `
 <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
              <div> <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
             </div>
         <div class="weather-forecast-temps">
