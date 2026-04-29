ddd840eb78f23515a28b47496d58fad09793de06
rebeccacorrinne <rebeccacorrinne@gmail.com>
2026-04-29 18:43:38 -0400

    added api for icons
    and formatted the array

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
 temperatureElement.innerHTML = Math.round(temperature);  
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
 
 getForecast(response.data.city);
 }
@@ -56,29 +55,27 @@ axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-console.log(response.data);
 
-let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 let forecastHtml = "";
 
-
-days.forEach(function (day) {
+response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
 forecastHtml = 
 forecastHtml + `
 <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-             <div class="weather-forecast-icon">
-        <img src="${day.condition.icon_url}" class="forecast-icon" /> 
+    <div class="weather-forecast-date">Tue</div>
+             <div> <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
             </div>
         <div class="weather-forecast-temps">
-        <div class="weather-forecast-temp">19°</div>
-        <div class="weather-forecast-temp">4°</div>
+        <div class="weather-forecast-temp">${Math.round(day.temperature.maximum)}°</div>
+        <div class="weather-forecast-temp">${Math.round(day.temperature.minimum)}°</div>
          </div>
     </div>
 </div>
     `;
+    }
     });
-``
+
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
 }
@@ -87,4 +84,3 @@ let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kyoto");
-displayForecast();
